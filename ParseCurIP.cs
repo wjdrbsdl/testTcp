e61ea3ff18177b6c97c3caf6bfaf550a2c5649d9
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-02-13 15:56:01 +0900

    현재 IP 파싱 클래스

---
+++ ParseCurIP.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ParseCurIP
+{
+    public static string GetLocalIP()
+    {
+        string result = string.Empty;
+
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                result = ip.ToString();
+                return result;
+            }
+
+           
+        }
+        return null;
+    }
+}
