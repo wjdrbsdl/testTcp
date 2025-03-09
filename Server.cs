369e1a904eb9d187b51807a6f5993dafc898b11c
YiYop <wjdrbsdl1011@naver.com>
2025-03-09 18:50:18 +0900

    [추가] 서버에서 방 다루기 1. 요청, 응답 표기할 enum 추가 2. 요청에 응답할 함수 추가 HandleRoomMaker

--- Server.cs
+++ Server.cs
@@ -6,13 +6,25 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
+
+public enum ReqType
+{
+    RoomMake, RoomReady, RoomOut
+}
+
+public enum MeetState
+{
+    Lobby, Room, Game
+}
+
+
 namespace testTcp
 {
     public class Server
     {
         int start = 5;
         static int index = 5;
-        public static int bufferSize = 255;
+        public static int bufferSize = 4000;
         Socket mainSock;
         List<Socket> connectedClients = new List<Socket>();
         List<AsyncObject> connectedClientList = new List<AsyncObject>();
@@ -21,7 +33,7 @@ namespace testTcp
         {
             try
             {
-                Console.WriteLine("서버 연결 시작");
+                Console.WriteLine("서버 연결 시작" + ParseCurIP.GetLocalIP());
                 mainSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                 IPEndPoint serverEP = new IPEndPoint(IPAddress.Any, m_port);
                 mainSock.Bind(serverEP);
@@ -99,13 +111,12 @@ namespace testTcp
                 int received = obj.WorkingSocket.EndReceive(ar);
                 byte[] buffer = new byte[received];
                 Array.Copy(obj.Buffer, 0, buffer, 0, received);
-                Console.WriteLine(obj.numbering + "클라로 부터 데이터 받음");
-
-                SendChat(obj.Buffer, obj.numbering);
+                //  SendChat(obj.Buffer, obj.numbering);
                 //Send(obj.Buffer);
+                HandleRoomMaker(obj, buffer);
 
                 obj.ClearBuffer();
-                obj.WorkingSocket.BeginReceive(obj.Buffer, 0, bufferSize, 0, DataReceived, obj);
+                obj.WorkingSocket.BeginReceive(obj.Buffer, 0, obj.BufferSize, 0, DataReceived, obj);
             }
             catch (Exception e)
             {
@@ -126,6 +137,41 @@ namespace testTcp
                 Console.WriteLine("서버에서이상"+  e.HResult);
             }
            
+        }
+
+        Dictionary<string, List<AsyncObject>> roomList = new();
+        void HandleRoomMaker(AsyncObject _obj, byte[] _reqData)
+        {
+            //
+            ReqType reqType = (ReqType)_reqData[0];
+            if(reqType == ReqType.RoomMake)
+            {
+                string roomName = Encoding.Unicode.GetString(_reqData, 1, _reqData.Length-1);
+                Console.WriteLine("신청한 방이름 "+ roomName);
+
+                /*
+                 * 방 만들기 리스폰스- 응답타입, 방참가 여부, 생성-참가 되었다면 참가자 수, 참가자 정보
+                 */
+                if(roomList.ContainsKey(roomName) == false)
+                {
+                    roomList.Add(roomName, new List<AsyncObject>());
+                }
+                roomList[roomName].Add(_obj);
+                byte[] parti = new byte[roomList[roomName].Count];
+                for (int i = 0; i < roomList[roomName].Count; i++)
+                {
+                    parti[i] = (byte)roomList[roomName][i].numbering;
+                }
+                byte[] roomCode = new byte[] { (byte)ReqType.RoomMake, 1, (byte)roomList[roomName].Count};
+
+                byte[] response = roomCode.Concat(parti).ToArray();
+
+                List<AsyncObject> pati = roomList[roomName];
+                for (int i = 0; i < pati.Count; i++)
+                {
+                    pati[i].WorkingSocket.Send(response);
+                }
+            }
         }
 
         public void SendChat(byte[] msg, int _receiveNumbering)
