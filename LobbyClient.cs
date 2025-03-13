fcc68d5ca541ec47aee867aea88b782a5e62bc81
YiYop <wjdrbsdl1011@naver.com>
2025-03-13 20:01:50 +0900

    [추가] 플레이 클라 생성시 로비 클라 아이디 전달

--- LobbyClient.cs
+++ LobbyClient.cs
@@ -143,7 +143,7 @@ public class LobbyClient
         clientSocket.Dispose();
 
         Console.WriteLine("플레이어 참가 클라이언트 생성");
-        PlayClient playerClient = new PlayClient(ip, 5001);
+        PlayClient playerClient = new PlayClient(ip, 5001, id);
         playerClient.Connect();
 
         meetState = MeetState.Room;
