ef7448f3fce41313b04dec76278b033874ab2bc7
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-03-13 13:43:47 +0900

    [추가] 최초 접속시 서버 ip 기록

--- ClientLogIn.cs
+++ ClientLogIn.cs
@@ -5,59 +5,58 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-    public class ClientLogIn
-    {
-        //클라로 접속을 했으면 여기서 아이피 입력하고 접속 대기 및 접속 ip 바꾸기 진행,
-        public void EnterIP()
-        {
-            
 
-            bool waitingInputIP = true;
-            
-            while (waitingInputIP)
+public class ClientLogIn
+{
+    public static byte[] ServerIp = new byte[] { };
+    //클라로 접속을 했으면 여기서 아이피 입력하고 접속 대기 및 접속 ip 바꾸기 진행,
+    public void EnterIP()
+    {
+        bool waitingInputIP = true;
+        while (waitingInputIP)
+        {
+            Console.WriteLine("접속할 서버 IP를 입력하세요 ex) 172.30.1.23");
+            string ip = Console.ReadLine();
+            //올바른 ip인지 체크 후
+            if (IsValidForm(ip) == true)
             {
-                Console.WriteLine("접속할 서버 IP를 입력하세요 ex) 172.30.1.23");
-                string ip = Console.ReadLine();
-                //올바른 ip인지 체크 후
-                if(IsValidForm(ip) == true)
-                {
-                    Console.Clear();
-                    RoomClient client = new RoomClient(ip, 1);
-                    client.Connect();
-                    waitingInputIP = false;
-                }
-                
+                Console.Clear();
+                RoomClient client = new RoomClient(ip, 1);
+                client.Connect();
+                waitingInputIP = false;
             }
-            
+
         }
 
-        private bool IsValidForm(string ip)
+    }
+
+    private bool IsValidForm(string ip)
+    {
+        //ipv4 가정
+        string[] intSplit = ip.Split(".");
+        //4구역이 나왔나
+        if (intSplit.Length != 4)
         {
-            //ipv4 가정
-            string[] intSplit = ip.Split(".");
-            //4구역이 나왔나
-            if (intSplit.Length != 4)
+            return false;
+        }
+        //모두 숫자이면서 0이상 255 아래인가
+        for (int i = 0; i < intSplit.Length; i++)
+        {
+            if (int.TryParse(intSplit[i], out int num) == false)
             {
                 return false;
             }
-            //모두 숫자이면서 0이상 255 아래인가
-            for (int i = 0; i < intSplit.Length; i++)
+
+            if (num < 0)
             {
-                if (int.TryParse(intSplit[i], out int num) == false)
-                {
-                    return false;
-                }
-
-                if (num < 0)
-                {
-                    return false;
-                }
-
-                if (255 < num)
-                {
-                    return false;
-                }
+                return false;
             }
-            return true;
+
+            if (255 < num)
+            {
+                return false;
+            }
         }
-   }
+        return true;
+    }
+}
