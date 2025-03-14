0384a164b0ed165f9c23ad76a7a1990691e7a0e1
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-03-14 10:16:43 +0900

    [추가] 카드데이터 클래스 1. 카드 기호와 숫자로 카드 생성

---
+++ CardData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public enum CardClass
+{
+    Space, Dia, Heart, Clover
+}
+
+public class CardData : Comparer<CardData>
+{
+    public CardClass cardClass = CardClass.Space;
+    public int num;
+
+
+    public CardData(CardClass _cardClass, int _num)
+    {
+        cardClass = _cardClass;
+        num = _num;
+    }
+
+    public int Compare(CardClass _class, int _num)
+    {
+        if (num == _num)
+        {
+            //무늬가 강한것부터 0으로 시작되어있어서, 뺏을 때의 값에 역순을 곱해서 크기 표현
+            return ((int)cardClass - (int)_class) * -1;
+        }
+
+        return num - _num;
+    }
+
+    public override int Compare(CardData? x, CardData? y)
+    {
+        if(x.num == y.num)
+        {
+            //무늬가 강한것부터 0으로 시작되어있어서, 뺏을 때의 값에 역순을 곱해서 크기 표현
+            return ((int)x.cardClass - (int)y.cardClass) * -1;
+        }
+
+        return x.num - y.num;
+    }
+}
+
