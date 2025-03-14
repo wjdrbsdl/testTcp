84c0088908ea788ed4fca24795ea400f93fb9fdf
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-03-14 14:36:30 +0900

    [추가] 카드 낱장 크기 비교 인터페이스

--- CardData.cs
+++ CardData.cs
@@ -11,7 +11,7 @@ public enum CardClass
     Spade, Dia, Heart, Clover
 }
 
-public class CardData : Comparer<CardData>
+public class CardData : Comparer<CardData>, IComparable<CardData>
 {
     public static CardClass minClass = CardClass.Spade;
     public static int minNum = 3;
@@ -36,8 +36,11 @@ public class CardData : Comparer<CardData>
         return num - _num;
     }
 
-    public int Compare(CardData _card)
+    public int CompareTo(CardData _card)
     {
+        if (_card == null)
+            return 0;
+
         if (num == _card.num)
         {
             //무늬가 강한것부터 0으로 시작되어있어서, 뺏을 때의 값에 역순을 곱해서 크기 표현
@@ -57,5 +60,6 @@ public class CardData : Comparer<CardData>
 
         return x.num - y.num;
     }
-}
+
+  }
 
