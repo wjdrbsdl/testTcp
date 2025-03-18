cb5f6afc03e9c0c740e5447f82d1349bb82de1f4
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-03-18 17:37:48 +0900

    [추가] 보유카드 인덱스, 낸카드 표기

--- Play/PlayClient.cs
+++ Play/PlayClient.cs
@@ -422,7 +422,7 @@ public class PlayClient
         for (int i = 0; i < haveCardList.Count; i++)
         {
             //i번째는 카드 무늬, i+1에는 카드 넘버가 있음
-            ColorConsole.Default($"보유 카드 {haveCardList[i].cardClass} : {haveCardList[i].num}");
+            ColorConsole.Default($"보유 카드 [{i}]{haveCardList[i].cardClass}:{haveCardList[i].num}");
         }
     }
 
@@ -523,7 +523,6 @@ public class PlayClient
         }
         //바닥에 깔린 카드 갱신
         ResetPutDownCard();
-        ColorConsole.Default(_data[1] + " 유저가 제출한 카드");
         for (int i = 3; i < _data.Length; i += 2)
         {
             CardClass cardClass = (CardClass)_data[i];
@@ -531,7 +530,10 @@ public class PlayClient
             CardData card = new CardData(cardClass, num); //카드 생성
             AddPutDownCard(card);
         }
-
+        CardRule rule = new CardRule();
+        rule.CheckValidRule(putDownList, out TMixture _mixture);
+        ColorConsole.Default($"{_data[1]}유저가 제출한 카드 {_mixture.mixture}:{_mixture.mainCardClass}:{_mixture.mainRealValue}");
+   
         //본인이 낸거라면 본인 카드에서 제외
         if (_data[1] == id)
         {
