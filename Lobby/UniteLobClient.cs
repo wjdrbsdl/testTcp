f409f46490610c26ff044817b1580f3fe4d2f3e9
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-03-26 12:45:23 +0900

    [추가] 접속실패 응답처리, 방 접속 코드 처리 1. 방접속 코드에서 방에 대한 패킷을 추려서 진행

--- Lobby/UniteLobClient.cs
+++ Lobby/UniteLobClient.cs
@@ -94,11 +94,31 @@ namespace testTcp.Lobby
                 ReqLobbyType reqType = (ReqLobbyType)recvData[0];
                 if (reqType == ReqLobbyType.RoomMake)
                 {
-                    ResRoomJoin(recvData);
+                    //
+                    //0번 제외하고 만들어서 넘기기
+                    byte[] roomDataByte = new byte[ recvData[1 + 0]]; //0번빼고 룸데이터 패킷에서 방 정보 총길이 index 0
+                    Buffer.BlockCopy(recvData, 1, roomDataByte, 0, roomDataByte.Length);
+                    /*
+                     * roomData 패킷 바이트
+                        * [0] 이 방 정보의 총 길이
+                        * [1] 룸의 현재 인원 - 0이면 자신이 방장
+                        * [2] 룸포트 길이 2
+                        * [3] 방 이름 길이 
+                        * [4] 3번부터 2번 만큼 길이를 ushort로 변환한거 - 포트번호
+                        * [4+[2]] 부터 [3] 길이 만큼이 방이름
+                    */
+
+                    ResRoomJoin(roomDataByte);
                 }
                 else if (reqType == ReqLobbyType.ClientNumber)
                 {
                     ResClientNumber(recvData);
+                }
+                else if (reqType == ReqLobbyType.RoomMakeFail)
+                {
+                    //방 실패
+                    ColorConsole.Default("방 접속 실패");
+                    ResRoomJoinFail();
                 }
 
                 if (clientSocket.Connected)
@@ -133,46 +153,36 @@ namespace testTcp.Lobby
                   * [3] 방 이름 길이 
                   * [4] 4번부터 2번 만큼
                   * [4+[2]] 부터 [3] 만큼
-                  */
+            */
 
-            if (_receiveData[1] == LobbyServer.failCode)
-            {
-                //현재 인원수 쪽에 패일 코드를 넣어서 불가 체크
-                ColorConsole.Default("방에 참가 못했음");
-                Update(); //참가신청하는 순간 입력 while문이 꺼져서 다시 켜주기
-                return;
-            }
+            RoomData roomData = new RoomData(_receiveData);
 
-            //아이피 주소 파싱
-            int portLengthIdx = 2;
-            int portLength = _receiveData[portLengthIdx];
-            byte[] portByte = new byte[portLength];
-            int portIdx = 4;
-            for (int i = 0; i < portLength; i++)
-            {
-                portByte[i] = _receiveData[i + portIdx];
-            }
             //포트 번호 생성
-            ushort portNum = (ushort)BitConverter.ToUInt16(portByte);
+            ushort portNum = (ushort)roomData.portNum;
 
-            //방이름 파싱
-            int roomNameLength = _receiveData[3];
-            string roomName = Encoding.Unicode.GetString(_receiveData, portIdx + portLength, _receiveData[3]);
-
-            ColorConsole.Default(roomName + "방에서 내 순서 :" + _receiveData[1].ToString());
+            string roomName = roomData.roomName;
+            ColorConsole.Default(roomName + "방에서 내 순서 :" + roomData.curCount);
 
             isLobby = false; //게임으로 이동
-       
+
             ColorConsole.Default("로비 클라 디스컨넥");
             ReqDisConnect();
             clientSocket.Close();//기존 소켓은 끊고 해당 클래스는 지움 
             clientSocket.Dispose();
 
             ColorConsole.Default("로비에서 플레이어 참가 클라이언트 생성 포트 번호 : " + portNum);
-            PlayClient playerClient = new PlayClient(UniteServer.ServerIp.GetAddressBytes() , portNum, id);
+            PlayClient playerClient = new PlayClient(UniteServer.ServerIp.GetAddressBytes(), portNum, id);
             playerClient.Connect();
 
             meetState = MeetState.Room;
+        }
+
+        private void ResRoomJoinFail()
+        {
+            //현재 인원수 쪽에 패일 코드를 넣어서 불가 체크
+            ColorConsole.Default("방에 참가 못했음");
+            Update(); //참가신청하는 순간 입력 while문이 꺼져서 다시 켜주기
+            return;
         }
         #endregion
 
